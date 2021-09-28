7b7c2839963244067a996f805d487bb77b3d397f
Maxwell Meyer <maxreadswell@gmail.com>
2021-09-28 10:52:58 -0700

    remove files, fix path errors to resolve compilation errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import "./App.css";
 import Headlines from "./Headlines";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Headlines />
-    </React.Fragment>
+    </>
   );
 }
 
